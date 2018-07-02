a9b0fd33aa24fac937a2648a70b8e495341f9813
GirishBodhe <bodhegirish@gmail.com>
2018-07-02 19:41:33 +0530

    Swift-Kuery Postgresql DB created localy
    https://github.com/IBM-Swift/Swift-Kuery

--- Package.swift
+++ Package.swift
@@ -9,10 +9,11 @@ let package = Package(
       .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "7.1.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
       .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
+      .package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL.git", from: "1.0.0"),
     ],
     targets: [
       .target(name: "KituraFirstDBApp", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
-      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics","Health",      ]),
+      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics","Health", "SwiftKueryPostgreSQL",    ]),
 
       .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura","HeliumLogger" ])
     ]
--- Sources/KituraFirstDBApp/main.swift
+++ Sources/KituraFirstDBApp/main.swift
@@ -3,11 +3,39 @@ import Kitura
 import LoggerAPI
 import HeliumLogger
 import Application
+import SwiftKuery
+import SwiftKueryPostgreSQL
 
 do {
-
+   
+    let pool = PostgreSQLConnection.createPool(host: "localhost", port: 5432, options: [.databaseName("school")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
+//    Database.default = Database(pool)
+    
+    
     HeliumLogger.use(LoggerMessageType.info)
 
+    class Grades: Table {
+        let tableName = "grades"
+        let id = Column("id", Int32.self, primaryKey: true)
+        let course = Column("course", String.self)
+        let grade = Column("grade", Int32.self)
+    }
+    let grades = Grades()
+//    let pool = PostgreSQLConnection.createPool(host: "localhost", port: 8080, options: [.databaseName("school")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
+    let students: [[Any]] = [[0, "computing", 921], [1, "physics", 751], [2, "history", 813]]
+    
+    if let connection = pool.getConnection() {
+        let insertQuery = Insert(into: grades, rows: students)
+        connection.execute(query: insertQuery) { insertResult in
+            connection.execute(query: Select(from: grades)) { selectResult in
+                if let resultSet = selectResult.asResultSet {
+                    for row in resultSet.rows {
+                        print("Student \(row[0] ?? ""), studying \(row[1] ?? ""), scored \(row[2] ?? "")")
+                    }
+                }
+            }
+        }
+    }
     let app = try App()
     try app.run()
 
