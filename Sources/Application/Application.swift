1fe18f4f8ae4784dc279b23245b3849f5b7a825d
GirishBodhe <bodhegirish@gmail.com>
2018-07-09 15:45:07 +0530

    Db test

--- Sources/Application/Application.swift
+++ Sources/Application/Application.swift
@@ -5,18 +5,31 @@ import Configuration
 import CloudEnvironment
 import KituraContracts
 import Health
+import SwiftKuery
+import SwiftKueryPostgreSQL
 
 public let projectPath = ConfigurationManager.BasePath.project.path
 public let health = Health()
 
+class Grades: Table {
+    let tableName = "grades"
+    let id = Column("id", Int32.self, primaryKey: true)
+    let course = Column("course", String.self)
+    let grade = Column("grade", Int32.self)
+}
+
 public class App {
     let router = Router()
     let cloudEnv = CloudEnv()
-
+    let grades = Grades()
+    
     public init() throws {
         // Run the metrics initializer
         initializeMetrics(router: router)
         router.all(middleware: BodyParser())
+       
+        
+      
     }
 
     func postInit() throws {
@@ -25,7 +38,28 @@ public class App {
         
         // Handle HTTP GET requests to "/"
         router.get("/") { request, response, next in
-            response.send("PrideVel..!!")
+        
+            let pool = PostgreSQLConnection.createPool(url:NSURL(fileURLWithPath:"zfdtmyn:9c4b553e93629efbfd8fc93cf2bf0b680a45f28bb7b776741d431b24bf456cc6@ec2-107-21-236-219.compute-1.amazonaws.com:5432/dajoh4aerlt4c1") as URL, poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
+            
+            let randomNum:UInt32 = arc4random_uniform(100) // range is 0 to 99
+            // convert the UInt32 to some other  types
+            let someInt:Int = Int(randomNum)
+            
+            let students: [[Any]] = [[3, "computing", someInt], [4, "physics", someInt], [5, "history", someInt]]
+            
+            if let connection = pool.getConnection() {
+                let insertQuery = Insert(into: self.grades, rows: students)
+                connection.execute(query: insertQuery) { insertResult in
+                    connection.execute(query: Select(from: self.grades)) { selectResult in
+                        if let resultSet = selectResult.asResultSet {
+                            for row in resultSet.rows {
+                                response.send("Student: \(row[0] ?? ""), studying: \(row[1] ?? ""), scored: \(row[2] ?? "")\n")
+                            }
+                        }
+                    }
+                }
+            }
+//            response.send("PrideVel..!!")
             next()
         }
         
--- Sources/KituraFirstDBApp/main.swift
+++ Sources/KituraFirstDBApp/main.swift
@@ -8,37 +8,36 @@ import SwiftKueryPostgreSQL
 
 do {
    
-//    let pool = PostgreSQLConnection.createPool(host: "localhost", port: 5432, options: [.databaseName("school")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
-//    Database.default = Database(pool)
-    
-    
+
     HeliumLogger.use(LoggerMessageType.info)
 
-    class Grades: Table {
-        let tableName = "grades"
-        let id = Column("id", Int32.self, primaryKey: true)
-        let course = Column("course", String.self)
-        let grade = Column("grade", Int32.self)
-    }
-    let grades = Grades()
-//    let pool = PostgreSQLConnection.createPool(host: "localhost", port: 5432, options: [.databaseName("school")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
-    
-    let pool = PostgreSQLConnection.createPool(url:NSURL(fileURLWithPath:"zfdtmyn:9c4b553e93629efbfd8fc93cf2bf0b680a45f28bb7b776741d431b24bf456cc6@ec2-107-21-236-219.compute-1.amazonaws.com:5432/dajoh4aerlt4c1") as URL, poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
-    
-    let students: [[Any]] = [[0, "computing", 921], [1, "physics", 751], [2, "history", 813]]
-    
-    if let connection = pool.getConnection() {
-        let insertQuery = Insert(into: grades, rows: students)
-        connection.execute(query: insertQuery) { insertResult in
-            connection.execute(query: Select(from: grades)) { selectResult in
-                if let resultSet = selectResult.asResultSet {
-                    for row in resultSet.rows {
-                        print("Student \(row[0] ?? ""), studying \(row[1] ?? ""), scored \(row[2] ?? "")")
-                    }
-                }
-            }
-        }
-    }
+//    class Grades: Table {
+//        let tableName = "grades"
+//        let id = Column("id", Int32.self, primaryKey: true)
+//        let course = Column("course", String.self)
+//        let grade = Column("grade", Int32.self)
+//    }
+//    let grades = Grades()
+//
+//    
+//    //    let pool = PostgreSQLConnection.createPool(host: "localhost", port: 5432, options: [.databaseName("school")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
+//    
+//    let pool = PostgreSQLConnection.createPool(url:NSURL(fileURLWithPath:"zfdtmyn:9c4b553e93629efbfd8fc93cf2bf0b680a45f28bb7b776741d431b24bf456cc6@ec2-107-21-236-219.compute-1.amazonaws.com:5432/dajoh4aerlt4c1") as URL, poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
+//    
+//    let students: [[Any]] = [[0, "computing", 921], [1, "physics", 751], [2, "history", 813]]
+//    
+//    if let connection = pool.getConnection() {
+//        let insertQuery = Insert(into: grades, rows: students)
+//        connection.execute(query: insertQuery) { insertResult in
+//            connection.execute(query: Select(from: grades)) { selectResult in
+//                if let resultSet = selectResult.asResultSet {
+//                    for row in resultSet.rows {
+//                        print("Student \(row[0] ?? ""), studying \(row[1] ?? ""), scored \(row[2] ?? "")")
+//                    }
+//                }
+//            }
+//        }
+//    }
     let app = try App()
     try app.run()
 
